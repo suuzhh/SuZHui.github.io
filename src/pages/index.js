03bf732755b75fedf058d24ddfe9a6c6676dbaab
suzh <362680581@qq.com>
2019-06-20 18:17:16 +0800

    [update]

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ const IndexPage = () => (
     <Link to="/contact/">Contact</Link>
     <Header headerText="Hello World!" />
     <p>What a world.</p>
-    <img src="https://source.unsplash.com/random/400x200" alt="" />
+    {/* <img src="https://source.unsplash.com/random/400x200" alt="" /> */}
   </div>
 );
 
