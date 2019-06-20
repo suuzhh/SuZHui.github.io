c9e9475eece7aa4bafcf290d1a2d80dcdbf7d036
suzh <362680581@qq.com>
2019-06-20 16:47:08 +0800

    [update] test deploy

---
+++ src/pages/about.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Header from '../components/header';
+
+const AboutPage = () => (
+    <div style={{ color: `teal` }}>
+        <Header headerText="About Me" />
+        <p>Such wow, Very React.</p>
+    </div>
+);
+
+export default AboutPage;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,14 @@
-import React from "react"
-import { Link } from "gatsby"
-
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import React from "react";
+import { Link } from 'gatsby';
+import Header from '../components/header';
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi2 people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+  <div style={{ color: `purple` }}>
+    <Link to="/contact/">Contact</Link>
+    <Header headerText="Hello World!" />
+    <p>What a world.</p>
+    <img src="https://source.unsplash.com/random/400x200" alt="" />
+  </div>
+);
 
-export default IndexPage
+export default IndexPage;
