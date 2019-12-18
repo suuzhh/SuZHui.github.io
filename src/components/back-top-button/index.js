ac0776a77a73553546238b926b84d599719bcb34
suzh <362680581@qq.com>
2019-12-18 10:20:11 +0800

    update

--- src/components/back-top-button/index.js
+++ src/components/back-top-button/index.js
@@ -1,35 +1,39 @@
 import React, { useState, useImperativeHandle, forwardRef } from 'react';
+import { CSSTransition } from "react-transition-group";
 import Icon, { ICON_BACK_TOP } from '../icon';
 
 const BackTopButton = forwardRef(({ onButtonClick }, ref) => {
 
-    const [ visible, handleVisible ] = useState(false);
+	const [ visible, handleVisible ] = useState(false);
 
-    useImperativeHandle(ref, () => ({
-        show() {
-            if (visible === false) {
-                handleVisible(true);
-            }
-        },
-        hide() {
-            if (visible === true) {
-                handleVisible(false);
-            }
-        }
-    }))
+	useImperativeHandle(ref, () => ({
+		show() {
+			if (visible === false) {
+				handleVisible(true);
+			}
+		},
+		hide() {
+			if (visible === true) {
+				handleVisible(false);
+			}
+		}
+	}))
 
-    return (
-        visible ?
-            <div className='dn db-ns pa2 pb1 pointer br-100 fixed right-2 bottom-2 mb4 shadow-1 flex items-center justify-center'
-            >
-                <div className='animation-slide orange link'
-                    onClick={onButtonClick}
-                >
-                    <Icon size={26} name={`${ICON_BACK_TOP}`}/>
-                </div>
-            </div> 
-            : null
-    );
+	return (
+		<CSSTransition
+			in={visible} timeout={400} classNames='fade'
+			mountOnEnter
+		>
+			<div className='dn db-ns pa2 pb1 pointer br-100 fixed right-2 bottom-2 mb4 shadow-1 flex items-center justify-center'
+			>
+				<div className='animation-slide orange link'
+					onClick={onButtonClick}
+				>
+					<Icon size={26} name={`${ICON_BACK_TOP}`}/>
+				</div>
+			</div> 
+		</CSSTransition>
+	);
 });
 
 export default BackTopButton;
